4b01318e1750966fa4d567103d8a4773255b8651
이재준 <jayl2cu@gmail.com>
2022-10-24 17:29:08 +0900

    ✨ ADD CORS Settings FastAPI
    - add cors settings in FastAPI

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI 
 from youtube_transcript_api import YouTubeTranscriptApi
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["*"]
+
 
 app = FastAPI() 
 
-@app.post("/api/v1/video/script/{video_id}") 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/v1/video/script/{video_id}") 
 async def root(video_id): 
     return YouTubeTranscriptApi.get_transcript(video_id)
 
