ab2bfc0328818bbf455a34d3b201e8b7504a5988
김누리 <zsda3131@naver.com>
2022-10-24 17:01:05 +0900

    ✨ 회원가입 유효성 검사
    - 회원가입 유효성 검사

--- frontend/src/models/user.ts
+++ frontend/src/models/user.ts
@@ -1,3 +1,7 @@
 export interface User {
-  userId: number | string;
-}
+  // id?: string;
+  email: string;
+  password: string;
+  // gender: 'male' | 'female';
+
+} //일단 사용하는데는 없음 비워놓으면 에러뜨네
--- frontend/src/pages/User/Signup/SignupForm.tsx
+++ frontend/src/pages/User/Signup/SignupForm.tsx
@@ -3,18 +3,35 @@ import styled from 'styled-components';
 
 import { useState, useEffect } from 'react';
 
-
-import AuthInputs from '../AuthInputs';
 import { useAppDispatch } from '../../../utils/hooks';
 import { authActions } from '../../../features/auth/authSlice';
 
-const StyledForm = styled.div``;
+//form 관리 라이브러리
+import { useForm } from 'react-hook-form';
+import { InputField } from '../InputField';
+import { RadioField } from '../RadioField';
 
-const SignupForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirm, setPasswordConfirm] = useState('');
-  const [errMsg, setErrMsg] = useState('');
+//유효성평가 라이브러리
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+interface IAuthForm {
+  nickname: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  gender: 'male' | 'female' | 'secret'
+}
+
+interface ISignupFormProps {
+  initialValues?: IAuthForm;
+  onSubmit?: (formValues: IAuthForm) => void;
+}
+
+const StyledForm = styled.form``;
+
+const SignupForm = ({ initialValues, onSubmit }: ISignupFormProps) => {
+  // const [errMsg, setErrMsg] = useState('');
 
   // 아이디, 비밀번호 정규식
   const userIdRegExp = /^[a-z0-9]{4,16}$/; //4자 이상, 16자 이하의 영문 혹은 숫자로 입력해주세요.
@@ -22,56 +39,77 @@ const SignupForm = () => {
 
   const dispatch = useAppDispatch();
 
+  const schema = yup.object().shape({
+    email: yup.string().required('이메일을 입력해주세요'),
+    gender: yup.string(),
+  });
 
-  // const idChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setEmail(e.target.value);
-  // };
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IAuthForm>({
+    defaultValues: initialValues,
+    resolver: yupResolver(schema),
+  });
+  const handleFormSubmit = (formValues: IAuthForm) => {
+    try {
+      onValid?.(formValues);
 
-  // const inputIdChangeHandler = (v: string) => {
-  //   console.log()
-  //   setEmail(v)
+      let { email, password } = formValues;
+      const signupInfo = {
+        email: email,
+        password: password,
+      };
 
-  // };
-
-  const inputIdChangeHandler = (v: string) => {
-    console.log()
-    setEmail(v)
-
+      dispatch(authActions.signup(signupInfo));
+      // await onSubmit?.(formValues);
+    } catch (error) {
+      console.log('에러가뭘까용', error);
+      // setError(error.message);
+    }
   };
 
-  const inputPasswordChangeHandler = (v: string) => {
-    setPassword(v)
-
-  };
-  
-
-  const formSubmitHandler = () => {
-    const signupInfo = {
-      email: email,
-      password: password,
-    };
-    dispatch(authActions.signup(signupInfo));
-    // e.preventDefault() //폼태그는 버튼 눌렀을 때 preventDefault를 해주지 않으면 새로고침됨
-    
+  const onValid = (data: IAuthForm) => {
+    if (data.password !== data.passwordConfirm) {
+      setError(
+        'passwordConfirm', // 에러 핸들링할 input요소 name
+        { message: '비밀번호가 일치하지 않습니다.' }, // 에러 메세지
+        { shouldFocus: true } // 에러가 발생한 input으로 focus 이동
+      );
+    }
   };
 
   return (
-    <StyledForm>
-      <AuthInputs
-        label='이메일'
-        type="text"
-        name="email"
-        value={email}
-        onInputChange={inputIdChangeHandler}
+    <StyledForm onSubmit={handleSubmit(handleFormSubmit)}>
+      <InputField name="email" control={control} label="이메일" />
+      <InputField name="password" control={control} label="비밀번호" />
+      <InputField
+        name="passwordConfirm"
+        control={control}
+        label="비밀번호 확인"
       />
-      <AuthInputs
-        label='비밀번호'
-        type="text"
-        name="password"
-        value={password}
-        onInputChange={inputPasswordChangeHandler}
+      <RadioField
+        name="gender"
+        control={control}
+        label="Gender"
+        options={[
+          {
+            label: '남성',
+            value: '0',
+          },
+          {
+            label: '여성',
+            value: '1',
+          },
+          {
+            label: '쉿,비밀이야',
+            value: '2',
+          },
+        ]}
       />
-      <button onClick={formSubmitHandler}>제출</button>
+      <button>제출</button>
     </StyledForm>
   );
 };
