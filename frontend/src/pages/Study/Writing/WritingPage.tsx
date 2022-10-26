22d774d4ae1f9933f2ecc9511c9955412c7f0259
donguk3105 <donguk3105@naver.com>
2022-10-26 22:34:45 +0900

    ✨ 라이팅페이지 레이아웃 작
    - 라이팅 페이지 레이아웃 작성1
    Close #https://ssafy.atlassian.net/browse/S07P31E202-125

--- frontend/src/pages/Study/Writing/WritingPage.tsx
+++ frontend/src/pages/Study/Writing/WritingPage.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import {
+  WritingPageBlock,
+  WordListAndWritingContainer,
+  WordListRegion,
+  WritingRegion,
+  BtnsRegion,
+  SaveBtn,
+  WritingTextArea,
+  ContentBox,
+  ContentBoxTag
+} from '../../../styles/Writing/WritingStyle';
 
 const WritingPage = () => {
   return (
-    <div>
-      <p>라이팅페이지입니다</p>
-    </div>
+    <WritingPageBlock>
+      <WordListAndWritingContainer>
+        <WordListRegion>
+          <ContentBox bgColor="white" widthSize="15vw" heightSize='35vh'>
+            <ContentBoxTag topShift='-4vh' leftShift='0vw' bgColor="white" fontColor='#005e76'>단어장</ContentBoxTag>
+          </ContentBox>
+          <ContentBox bgColor="#005e76" widthSize="15vw" heightSize='25vh'>
+            <ContentBoxTag topShift='-4vh' leftShift='0vw' bgColor="#005e76" fontColor='white'>사전</ContentBoxTag>  
+          </ContentBox>          
+        </WordListRegion>
+        <WritingRegion>
+          <WritingTextArea></WritingTextArea>
+        </WritingRegion>
+      </WordListAndWritingContainer>
+      <BtnsRegion>
+        <SaveBtn>파일로 저장</SaveBtn>
+        <SaveBtn>저장</SaveBtn>
+      </BtnsRegion>
+    </WritingPageBlock>
   );
 };
 
---
+++ frontend/src/styles/Writing/WritingStyle.ts
@@ -0,0 +1,117 @@
+import styled from "styled-components";
+
+interface ContentBoxProps {
+  bgColor: string;
+  widthSize: string;
+  heightSize: string;
+}
+
+interface ContentBoxTagProps {
+  topShift: string;
+  leftShift: string; 
+  bgColor: string;
+  fontColor: string;
+}
+
+export const WritingPageBlock = styled.div`
+  display: flex;
+  /* justify-content: center; */
+  /* align-items: center; */
+  flex-direction: column;
+  width: 80vw;
+  height: 85vh;
+  margin: 0 10vw;
+  /* border: 1px solid black; */
+`;
+
+export const WordListAndWritingContainer = styled.div`
+  display: flex;
+  width: 80vw;
+  height: 70vh;  
+  /* border: 1px solid blue; */
+  margin-bottom: 2vw;
+`;
+
+export const WordListRegion = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;  
+  background-color: #daedf2;
+  border-radius: 25px;  
+  width: 17vw;
+  height: 70vh;
+  margin-right: 3vw;
+  /* border: 1px solid green; */
+`;
+
+export const ContentBox = styled.div<ContentBoxProps>`
+  display: flex;
+  position: relative;
+  background-color: ${(props) => props.bgColor};
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;  
+  width: ${props => props.widthSize};
+  height: ${props => props.heightSize};  
+  /* border: 1px solid green; */
+`;
+
+export const ContentBoxTag = styled.div<ContentBoxTagProps>`  
+  position: absolute;
+  top:${(props) => props.topShift};   
+  left:${(props) => props.leftShift};  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 5vw;
+  height: 4vh;
+  background-color: ${(props) => props.bgColor};;
+  border-top-right-radius: 10px;
+  border-top-left-radius: 10px;
+  font-size: 2vmin;
+  color: ${(props) => props.fontColor};
+  /* border: 1px solid green; */
+`;
+
+export const WritingRegion = styled.div`
+  display: flex;
+  background-color: #daedf2;
+  border-radius: 25px;
+  width: 60vw;
+  height: 70vh; 
+  padding: 2vmin;
+  /* border: 1px solid yellow; */
+`;
+
+export const WritingTextArea = styled.textarea`
+  display: flex;
+  background-color: white;
+  border-radius: 25px;
+  width: 100%;
+  height: 100%;
+  padding: 2vmin 3vmin;
+  font-size: 2vmin;
+  border: none;
+  resize: none;
+`;
+
+export const BtnsRegion = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 20vw;
+  margin-left: 60vw;
+  height: 5vh;  
+  /* border: 1px solid darkcyan; */
+`
+
+export const SaveBtn = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 9vw;  
+  height: 5vh;  
+  font-size: 2.8vmin;
+  border-radius: 10px;
+  background-color: navy;
+  color: white;
+`
