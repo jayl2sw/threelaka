cbbac1be3f9ad9d310b76d6d8071dbab64c1a3cc
hyunspace <33j073@gmail.com>
2022-11-03 12:14:22 +0900

    ✨ RecommendVideos styling
    - 추천 영상 스타일링 시작

--- frontend/src/pages/Main/components/VideoCard.tsx
+++ frontend/src/pages/Main/components/VideoCard.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+
+// style
 import {
   VideoCardBlock,
   VideoImg,
   SubTagContainer,
   SubTag,
 } from '../../../styles/Main/MainStyle';
+import { MainBtn } from '../../../styles/Common/CommonBtnStyle';
+import { MainBox } from '../../../styles/Common/CommonDivStyle';
 
 // video별 갖고 있는 정보들
 // 이것도 interface?? 모르겠어 9ㅅ9
