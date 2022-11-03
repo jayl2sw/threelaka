e76eea4f71132b76467ed8425eed7febded91f16
Choi Jiwon <c.jiwon3000@gmail.com>
2022-11-03 22:19:05 +0900

    📝 Fix WordBook CSS
    css for wordbook in writing page

--- frontend/src/styles/Writing/WritingStyle.ts
+++ frontend/src/styles/Writing/WritingStyle.ts
@@ -29,6 +29,8 @@ export const WordCheckBox = styled.div`
   background-color: #9897a9;
   margin-right: 1vw;
   border-radius: 1vmin;
+  position: absolute;
+  top: 1vh;
   &.checked {
     background: linear-gradient(
       106.62deg,
@@ -58,14 +60,14 @@ export const HighlightTextAreaWrapper = styled.div`
 export const WordText = styled.div`
   width: 20vw;
   display: flex;
+  margin-left: 5vmin;
+  padding-right: 2vmin;
+  position: absolute;
+  top: -1.5vh;
   & p.back {
-    display: none;
-  }
-  &:hover p.front {
-    display: none;
-  }
-  &:hover p.back {
-    display: inline;
+    opacity: 0;
+    height: 0;
+    transition: 0s;
   }
   & p.checked {
     text-decoration: black line-through;
@@ -73,20 +75,34 @@ export const WordText = styled.div`
 `;
 
 export const ArcodianBox = styled.div`
+  position: relative;
   display: flex;
   justify-content: start;
   align-items: center;
   margin-bottom: 1vh;
   width: 25.3vw;
   height: 5vh;
-  padding: 1vw;
+  padding: 0 1vw;
   font-size: 1vmin;
   color: black;
   background: #ffffff;
   box-shadow: 10px 10px 80px rgba(63, 39, 102, 0.1);
   border-radius: 2vmin;
+  overflow-x: hidden;
+  overflow-y: scroll;
   &:hover {
-    height: 40vh;
+    height: 20vh;
   }
-  transition: height ease-in-out 0.8s;
+  &:hover > div:nth-child(2) {
+    top: 0;
+  }
+  &:hover p.front {
+    display: none;
+  }
+  &:hover p.back {
+    opacity: 1;
+    height: 100%;
+    transition: 2s;
+  }
+  transition: 0.8s;
 `;
