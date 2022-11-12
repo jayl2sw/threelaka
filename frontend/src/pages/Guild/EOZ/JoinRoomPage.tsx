6746306186f28252d23fa8f897d9f63645508813
김지현 <33j073@gmail.com>
2022-11-12 17:11:09 +0900

    ✨ 길드 Route 수정 및 EOZ 메인 컴포넌트 추가

---
+++ frontend/src/pages/Guild/EOZ/JoinRoomPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const JoinRoomPage = () => {
+  return <div></div>;
+};
+
+export default JoinRoomPage;
---
+++ frontend/src/pages/Guild/EOZ/ZonePage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ZonePage = () => {
+  return <div></div>;
+};
+
+export default ZonePage;
