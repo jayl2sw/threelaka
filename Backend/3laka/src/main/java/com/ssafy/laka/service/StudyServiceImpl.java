9e128689428a474dfc63bb87ca22b469f8dcc781
이재준 <jayl2cu@gmail.com>
2022-11-14 11:15:47 +0900

    🔧 FIX bugs
    - fix bugs

--- Backend/3laka/src/main/java/com/ssafy/laka/service/StudyServiceImpl.java
+++ Backend/3laka/src/main/java/com/ssafy/laka/service/StudyServiceImpl.java
@@ -165,7 +165,7 @@ public class StudyServiceImpl implements StudyService{
     @Override
     public List<WordbookResponseDto> getWordbooksByUser() {
         User user = SecurityUtil.getCurrentUsername().flatMap(userRepository::findByUsername).orElseThrow(UserNotFoundException::new);
-        translate("word");
+//        translate("word");
         return wordbookRepository.findWordbooksByUser(user).stream()
                 .map(w -> WordbookResponseDto.from(w)).collect(Collectors.toList());
 
@@ -272,5 +272,78 @@ public class StudyServiceImpl implements StudyService{
         LearningRecord lr = learningRecordRepository.findById(data.getLearningRecordId()).orElseThrow(LearningRecordNotFoundException::new);
         lr.setSurvey(data.getSurvey());
     }
-
+//    public String translate(String eng) {
+//
+//        String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+//        String text;
+//        try {
+//            text = URLEncoder.encode(eng, "UTF-8");
+//        } catch (UnsupportedEncodingException e) {
+//            throw new RuntimeException("인코딩 실패", e);
+//        }
+//
+//        Map<String, String> requestHeaders = new HashMap<>();
+//        requestHeaders.put("X-Naver-Client-Id", clientId);
+//        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+//
+//        String responseBody = post(apiURL, requestHeaders, text);
+//        System.out.println("responseBody = " + responseBody);
+//
+//
+//        return null;
+//    }
+//    private String post(String apiUrl, Map<String, String> requestHeaders, String text){
+//        HttpURLConnection con = connect(apiUrl);
+//        String postParams = "source=en&target=ko&text=" + text; //원본언어: 한국어 (ko) -> 목적언어: 영어 (en)
+//        try {
+//            con.setRequestMethod("POST");
+//            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+//                con.setRequestProperty(header.getKey(), header.getValue());
+//            }
+//
+//            con.setDoOutput(true);
+//            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+//                wr.write(postParams.getBytes());
+//                wr.flush();
+//            }
+//
+//            int responseCode = con.getResponseCode();
+//            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
+//                return readBody(con.getInputStream());
+//            } else {  // 에러 응답
+//                return readBody(con.getErrorStream());
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException("API 요청과 응답 실패", e);
+//        } finally {
+//            con.disconnect();
+//        }
+//    }
+//
+//    private HttpURLConnection connect(String apiUrl){
+//        try {
+//            URL url = new URL(apiUrl);
+//            return (HttpURLConnection)url.openConnection();
+//        } catch (MalformedURLException e) {
+//            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+//        } catch (IOException e) {
+//            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+//        }
+//    }
+//    private String readBody(InputStream body){
+//        InputStreamReader streamReader = new InputStreamReader(body);
+//
+//        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+//            StringBuilder responseBody = new StringBuilder();
+//
+//            String line;
+//            while ((line = lineReader.readLine()) != null) {
+//                responseBody.append(line);
+//            }
+//
+//            return responseBody.toString();
+//        } catch (IOException e) {
+//            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+//        }
+//    }
 }
