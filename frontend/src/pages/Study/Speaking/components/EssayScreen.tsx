591ef02031887556a56931b47535be8aee94f5c4
김누리 <zsda3131@naver.com>
2022-11-20 21:42:29 +0900

    ✨ 스피킹 진짜 완성
    - 스피킹 진짜 완성

--- frontend/src/pages/Study/Speaking/components/EssayScreen.tsx
+++ frontend/src/pages/Study/Speaking/components/EssayScreen.tsx
@@ -5,15 +5,16 @@ import { EssayContainer } from '../../../../styles/Speaking/SpeakingStyle';
 import EssayForTest from './EssayForTest';
 import EssayForSpeaking from './EssayForSpeaking';
 import { StudyPageParams } from '../../../../models';
+import { IEssayButtons } from './EssayButtons';
 import { studyActions } from '../../../../features/study/study-slice';
 
-const EssayScreen = () => {
+const EssayScreen = ({ essayOn, setEssayOn }: IEssayButtons) => {
   const modeCode = useLocation().search.replace('?mode=', '');
 
   // Essay for Test
   const pageParams: StudyPageParams = useParams() as any;
   const [selectedText, setSelectedText] = useState<string>('');
-  const [flag, setFlag] = useState<boolean>(false);
+
   const dispatch = useAppDispatch();
   // const speechScores = useAppSelector((state) => state.study.speechScores);
   // const totalScore = useAppSelector((state) => state.study.totalScore);
@@ -28,14 +29,10 @@ const EssayScreen = () => {
 
   if (modeCode === 'test') {
     return (
-      <EssayForTest
-        setSelectedText={setSelectedText}
-        pageParams={pageParams}
-        // setFlag={flag}
-      />
+      <EssayForTest setSelectedText={setSelectedText} pageParams={pageParams} />
     );
   } else {
-    return <EssayForSpeaking />;
+    return <EssayForSpeaking essayOn={essayOn} setEssayOn={setEssayOn} />;
   }
 };
 
