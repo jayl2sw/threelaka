92a1502a557bbfa814253e0148a79197d07b3410
donguk3105 <donguk3105@naver.com>
2022-11-20 23:51:48 +0900

    비디오스 페이지 80퍼 완성

---
+++ frontend/src/pages/Main/components/VideoCardTwo.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+// style
+import {
+  VideoCardBlock,
+  VideoDataBox,
+  VideoImg,
+  SubTagContainer,
+  SubTag,
+  VideoTitle,
+} from '../../../styles/Main/MainStyle';
+import { FlexTransparentDiv } from '../../../styles/Common/CommonDivStyle';
+
+// video별 갖고 있는 정보들
+interface VideoCardProps {
+  data: {
+    videoId: string;
+    title: string;
+    description: string;
+    korScript: boolean;
+  };
+  setModalToggleVideoId: (nextVideoId: string) => void;
+}
+
+const VideoCardTwo = ({ data, setModalToggleVideoId }: VideoCardProps) => {
+  // 영상 제목 정제하기 ('이름: 제목' 또는 '제목 | 이름'에서 제목만 남기기)
+  const cutIndex = data.title.indexOf('|');
+  const videoTitle =
+    cutIndex !== -1
+      ? data.title.substr(0, cutIndex)
+      : data.title.substr(data.title.indexOf(':') + 1);
+
+  return (
+    <VideoCardBlock
+      onClick={() => {
+        console.log('클릭');
+        setModalToggleVideoId(data.videoId);
+      }}
+      style={{ cursor: 'pointer' }}
+    >
+      <VideoDataBox>
+        <FlexTransparentDiv
+          widthSize={'19vw'}
+          heightSize={'22.5vh'}
+          paddingSize={'0'}
+          flexDirection={'column'}
+          justifyContent={'center'}
+          alignItems={'center'}
+          IsBorder={'none'}
+        >
+          <VideoImg
+            style={{
+              borderBottom: '3px solid grey',
+              borderTop: '3px solid grey',
+              borderRadius: '10px',
+            }}
+            src={`https://img.youtube.com/vi/${data.videoId}/0.jpg`}
+          />
+          <SubTagContainer>
+            {data.korScript && <SubTag>한글</SubTag>}
+            <SubTag>ENG</SubTag>
+          </SubTagContainer>
+        </FlexTransparentDiv>
+      </VideoDataBox>
+      <VideoTitle style={{ color: 'white' }}>{videoTitle}</VideoTitle>
+    </VideoCardBlock>
+  );
+};
+
+export default VideoCardTwo;
