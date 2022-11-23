9c3e2a73dc019ab918243f03ddfc119ee5385c29
김누리 <zsda3131@naver.com>
2022-11-23 14:30:37 +0900

    ✨ 녹화 수정
    - 녹화 수정

--- frontend/src/pages/Study/Speaking/components/WebCam.tsx
+++ frontend/src/pages/Study/Speaking/components/WebCam.tsx
@@ -41,7 +41,6 @@ const WebCam = () => {
       'dataavailable',
       handleDataAvailable
     );
-    console.log(webcamRef);
     mediaRecorderRef.current.start();
   }, [webcamRef, setRecording, mediaRecorderRef]);
 
@@ -67,8 +66,6 @@ const WebCam = () => {
 
   // 녹화된 영상 세팅
   const setRecordedVideo = useCallback(() => {
-    console.log('얍얍');
-    console.log(recordedChunks);
     if (recordedChunks.length) {
       const blob = new Blob(recordedChunks, {
         type: 'video/webm',
@@ -129,14 +126,7 @@ const WebCam = () => {
           <MdCameraswitch />
         </VideoAudioBtn>
         {recording ? (
-          <VideoAudioBtn
-            onClick={() => {
-              handleStopCaptureClick();
-              turnScreen();
-              setRecordedVideo();
-            }}
-            title="녹화 중지"
-          >
+          <VideoAudioBtn onClick={handleStopCaptureClick} title="녹화 중지">
             <BsFillStopFill />
           </VideoAudioBtn>
         ) : (
@@ -153,3 +143,114 @@ const WebCam = () => {
 };
 
 export default WebCam;
+
+// import React, { useEffect } from 'react';
+// import { RecordWebcam, useRecordWebcam } from 'react-record-webcam';
+
+// export default function WebCam() {
+//   const OPTIONS = {
+//     filename: 'test-filename',
+//     fileType: 'mp4',
+//     width: 1920,
+//     height: 1080,
+//   };
+//   const recordWebcam = useRecordWebcam(OPTIONS);
+//   const getRecordingFileHooks = async () => {
+//     const blob = await recordWebcam.getRecording();
+//     console.log({ blob });
+//   };
+
+//   const getRecordingFileRenderProp = async (blob: Blob) => {
+//     console.log({ blob });
+//   };
+
+//   useEffect(() => {
+//     recordWebcam.open();
+//   }, []);
+//   return (
+//     <div style={{ position: 'relative' }}>
+//       <div style={{
+//         visibility:recordWebcam.webcamRef?'hidden':''
+//       }}>Spacer</div>
+//       <video
+//         ref={recordWebcam.webcamRef}
+//         style={{
+//           display: `${
+//             recordWebcam.status === 'OPEN' ||
+//             recordWebcam.status === 'RECORDING'
+//               ? 'block'
+//               : 'none'
+//           }`,
+//           width: '45vw',
+//           height: '45vh',
+//         }}
+//         autoPlay
+//         muted
+//       />
+//       <video
+//         ref={recordWebcam.previewRef}
+//         style={{
+//           display: `${recordWebcam.status === 'PREVIEW' ? 'block' : 'none'}`,
+//           width: '40vw',
+//         }}
+//         controls
+//       />
+//       {/* <p>Camera status: {recordWebcam.status}</p> */}
+//       <div style={{ position: 'absolute' }}>
+//         <button
+//           disabled={
+//             recordWebcam.status === 'OPEN' ||
+//             recordWebcam.status === 'RECORDING' ||
+//             recordWebcam.status === 'PREVIEW'
+//           }
+//           onClick={recordWebcam.open}
+//         >
+//           Open camera
+//         </button>
+//         {/* <button
+//           disabled={
+//             recordWebcam.status === 'CLOSED' ||
+//             recordWebcam.status === 'PREVIEW'
+//           }
+//           onClick={recordWebcam.close}
+//         >
+//           Close camera
+//         </button> */}
+//         <button
+//           disabled={
+//             recordWebcam.status === 'CLOSED' ||
+//             recordWebcam.status === 'RECORDING' ||
+//             recordWebcam.status === 'PREVIEW'
+//           }
+//           onClick={recordWebcam.start}
+//         >
+//           Start recording
+//         </button>
+//         <button
+//           disabled={recordWebcam.status !== 'RECORDING'}
+//           onClick={recordWebcam.stop}
+//         >
+//           Stop recording
+//         </button>
+//         <button
+//           disabled={recordWebcam.status !== 'PREVIEW'}
+//           onClick={recordWebcam.retake}
+//         >
+//           Retake
+//         </button>
+//         <button
+//           disabled={recordWebcam.status !== 'PREVIEW'}
+//           onClick={recordWebcam.download}
+//         >
+//           Download
+//         </button>
+//         <button
+//           disabled={recordWebcam.status !== 'PREVIEW'}
+//           onClick={getRecordingFileHooks}
+//         >
+//           Get recording
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
